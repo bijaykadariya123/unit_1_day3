7d00702af4430cdb8507a557776df2998fdd6911
Bijay Kadariya <bijaykadariya@bijays-mbp.myfiosgateway.com>
2023-04-30 19:48:46 -0400

    FizzBUzz Answered

--- answers.js
+++ answers.js
@@ -26,11 +26,32 @@ for (i = 0; i <= 200; i++) {
 */
 
 ////////////////////////////////
+//==================Question no 2===============================
+//This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+/*
+for (i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+for (i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
+
 // Fizz Buzz
+*/
 ////////////////////////////////
 
 ////////////////////////////////
 // Wild Wild Life
+
 ////////////////////////////////
 
 ////////////////////////////////

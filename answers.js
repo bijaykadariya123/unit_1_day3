d16adf761a6d28fa6d90850c0b2ebb7de4b6b551
Bijay Kadariya <bijaykadariya@bijays-mbp.myfiosgateway.com>
2023-04-30 22:04:11 -0400

    Method Revisited answered

--- answers.js
+++ answers.js
@@ -52,11 +52,11 @@ for (i = 1; i <= 100; i++) {
 ////////////////////////////////
 // Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
 // plantee.splice(2, 1, 5001);
 // console.log(plantee);
@@ -83,11 +83,68 @@ const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 // for (let element of ninja_turtles) {
 //   console.log(element.toUpperCase());
 // }
-
 // Yell at the Ninja Turtles
 ////////////////////////////////
 
 ////////////////////////////////
+// const favMovies = [
+//   "Jaws",
+//   "The Fellowship of the Ring",
+//   "Howl's Moving Castle",
+//   "Django Unchained",
+//   "Cloud Atlas",
+//   "The Usual Suspects",
+//   "Toy Story",
+//   "Conan the Barbarian",
+//   "Titanic",
+//   "Harry Potter",
+//   "Fried Green Tomatoes",
+//   "Volver",
+//   "Oculus",
+//   "Seven",
+//   "Black Panther",
+//   "Harry Potter",
+//   "Imitation of Life",
+//   "Snatch",
+//   "Fast and Furious",
+// ];
+//Console log: the index of Titanic
+// console.log(favMovies.indexOf("Titanic"));
+// use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+//Answer: It arrange the array in ASCII order
+
+// Use the method pop
+// favMovies.pop()
+// push "Guardians of the Galaxy"
+// favMovies.push("Guardians")
+// Reverse the array
+// favMovies.reverse();
+
+// Use the shift method
+// favMovies.shift();
+
+// unshift - what does it return?
+//favMovies.unshift();: It doesnot add anything on the array
+// splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself) Thought question: did this permanently alter our array?
+// console.log(favMovies.indexOf("Django Unchained"));
+// favMovies.splice(3, 1, "Avatar");
+// console.log(favMovies);
+// slice the last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+// store the value of your slice in a variable, console.log it - Thought question: what is going on here?
+// console.log your final results
+// After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+// const middle_array_length = Math.floor(favMovies.length / 2);
+// const first_halfArray = favMovies.slice(0, middle_array_length);
+// const lastHalf_array = favMovies.slice(middle_array_length);
+// console.log(first_halfArray);
+// console.log(lastHalf_array);
+// console.log(first_halfArray.indexOf("Fast and Furious"));
+// No It doesnot permanently altrer our array
+//  We will get value of -1 if the element is not in array
+
+// Thought question: that we declared the variable favMovies with const, and yet, we were allowed to change the array. Weird? Should we have used let?
+//Elements of array are mutable even though the variable is assign with const how ever const variable reassin is not eligible for array
+
 // Methods, Revisited
 ////////////////////////////////
 
